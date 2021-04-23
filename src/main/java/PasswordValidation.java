71edc86b78121cddd51ba9810b8e2295ab295ef8
Fabian F <h-f.fromme@posteo.de>
2021-04-23 12:27:55 +0200

    Add method checkPasswordLength

--- src/main/java/PasswordValidation.java
+++ src/main/java/PasswordValidation.java
@@ -1,6 +1,16 @@
 public class PasswordValidation {
     public static void main(String[] args) {
-        @Param
+        String password = "Keks";
+        System.out.println(checkPasswordLength(password, 3));
+    }
+    public static boolean checkPasswordLength(String password, int passMinLength){
 
+        int minPasswordLength;
+        if(passMinLength > 0){
+            minPasswordLength = passMinLength;
+        } else {
+            minPasswordLength = 3;
+        }
+        return (password.length() >= minPasswordLength);
     }
 }
--- src/test/java/PasswordValidationTest.java
+++ src/test/java/PasswordValidationTest.java
@@ -7,6 +7,6 @@ public class PasswordValidationTest {
     @ParameterizedTest(name= "pass {0} should return pass length {1}")
     @CsvSource ({"a, false", "aa, false" , "aaa, true"})
     public void checkLength(String pass, boolean expected) {
-        assertEquals(expected, PasswordValidation.checkPasswordLength(pass));
+        assertEquals(expected, PasswordValidation.checkPasswordLength(pass, 0));
     }
 }
