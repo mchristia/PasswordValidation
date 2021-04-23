5f1fa83b96ffb4064fdfc557bfc501f5a616021f
[Mario] <[meins2320@gmail.com]>
2021-04-23 15:24:25 +0200

    Add method and test for lower and upper case

--- src/main/java/PasswordValidation.java
+++ src/main/java/PasswordValidation.java
@@ -1,8 +1,9 @@
 public class PasswordValidation {
     public static void main(String[] args) {
-        String password = "Bunny88";
-        System.out.println(checkPasswordLength(password, 3));
-        System.out.println(checkForNumber(password));
+        String password = "buNny88";
+//        System.out.println(checkPasswordLength(password, 3));
+//        System.out.println(checkForNumber(password));
+        System.out.println(checkForLowerUpperLetter(password));
     }
     public static boolean checkPasswordLength(String password, int passMinLength){
 
@@ -17,5 +18,18 @@ public class PasswordValidation {
 
     public static boolean checkForNumber(String password) {
         return password.matches(".*[0-9].*");
+    }
+
+    public static boolean checkForLowerUpperLetter(String password) {
+//        return password.matches("(?=.*[a-z])(?=.*[A-Z])");   //"(.*[a-z].*)(.*[A-Z].*)");
+        String regex1 = ".*[a-z].*";
+        String regex2 = ".*[A-Z].*";
+
+        if(password.matches(regex1 )&& password.matches(regex2)){
+            return true;
+        }else {
+            return false;
+        }
+
     }
 }
--- src/test/java/PasswordValidationTest.java
+++ src/test/java/PasswordValidationTest.java
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -15,4 +16,15 @@ public class PasswordValidationTest {
     public void checkForNumbers(String password, boolean hasNumber){
         assertEquals(hasNumber, PasswordValidation.checkForNumber(password));
     }
+
+    @ParameterizedTest(name= "pass {0} should return if lower or upper case exists {1}")
+    @CsvSource ({"Bunny88, true", "withoutnumber, false" , "Mr4lf4, true", "4Mr, true", "2, false", "A, false"})
+    public void checkForLowerUpperCase(String password, boolean hasLowerUpperLetter){
+        assertEquals(hasLowerUpperLetter, PasswordValidation.checkForLowerUpperLetter(password));
+    }
+
+//    @Test
+//    public void checkForLowerUpperCase(){
+//        assertEquals(true, PasswordValidation.checkForLowerUpperLetter("Bunny"));
+//    }
 }
