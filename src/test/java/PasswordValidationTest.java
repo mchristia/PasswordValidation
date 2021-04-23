2e8f72eda6caa0b32349e6c5bd0f3aca15fa2d5e
ole-na <oshalunova@yahoo.de>
2021-04-23 12:00:54 +0200

    Add simple init test for password min length

--- src/test/java/PasswordValidationTest.java
+++ src/test/java/PasswordValidationTest.java
@@ -1,5 +1,12 @@
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PasswordValidationTest {
-    @ParameterizedTest
+    @ParameterizedTest(name= "pass {0} should return pass length {1}")
+    @CsvSource ({"a, false", "aa, false" , "aaa, true"})
+    public void checkLength(String pass, boolean expected) {
+        assertEquals(expected, PasswordValidation.checkPasswordLength(pass));
+    }
 }
